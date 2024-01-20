36fbb6e7763bf5328c566060ced5cd48a720f9f1
Wansix <78299639+Wansix@users.noreply.github.com>
2024-01-20 10:40:06 +0000

    add register wallet page

--- src/screens/VerifyBamboo.js
+++ src/screens/VerifyBamboo.js
@@ -8,6 +8,7 @@ import Caver from "caver-js";
 import * as Crypto from "crypto";
 import axios from "axios";
 import dotenv from "dotenv";
+import Swal from "sweetalert2";
 
 dotenv.config();
 axios.defaults.withCredentials = true;
@@ -36,8 +37,8 @@ const verifyMessage = ({ message, address, signature }) => {
   const _address = ethers.utils.getAddress(address);
   const _signerAddr = ethers.utils.getAddress(signerAddr);
 
-  console.log("_address : ", _address);
-  console.log("_signerAddr : ", _signerAddr);
+  // console.log("_address : ", _address);
+  // console.log("_signerAddr : ", _signerAddr);
 
   if (_signerAddr !== _address) {
     return false;
@@ -53,9 +54,9 @@ const signMessage = async (walletName, message) => {
     const signature = await signer.signMessage(message);
     const address = await signer.getAddress();
 
-    console.log("address : ", address);
+    // console.log("address : ", address);
 
-    console.log("signature:", signature);
+    // console.log("signature:", signature);
 
     return signature;
   } else if (walletName === "kaikas") {
@@ -63,25 +64,31 @@ const signMessage = async (walletName, message) => {
     const account = window.klaytn.selectedAddress;
     const signature = await caver.klay.sign(message, account);
 
-    console.log("account : ", account);
-    console.log("sig : ", signature);
+    // console.log("account : ", account);
+    // console.log("sig : ", signature);
 
     return signature;
   }
 };
 
 export const VerifyBamboo = () => {
-  const [account, setAccount] = useState("");
+  const [account, setAccount] = useState();
   const [provider, setProvider] = useState();
   const [walletName, setWalletName] = useState("");
   const [signature, setSignature] = useState("");
   const [userData, setUserData] = useState();
+  const [registeredAddress, setRegisteredAddress] = useState("");
 
   const handleSign = async (e) => {
     e.preventDefault();
 
     if (!account) {
-      alert("지갑 연결 해주세요");
+      Swal.fire({
+        icon: "error",
+        title: "connect wallet",
+        text: "지갑 연결 해주세요",
+      });
+      // alert("지갑 연결 해주세요");
       return;
     }
     if (!provider) {
@@ -99,38 +106,34 @@ export const VerifyBamboo = () => {
     try {
       const sig = await handleSign(e);
 
+      if (sig) {
+        const message = "bambooSoop verify";
+
+        await registerWallet(userData, message, sig, account, walletName);
+      }
+
       // const _balanceNFT = Number(await readContract.balanceOf(account));
       // console.log("balance ", _balanceNFT);
 
-      console.log("w:", walletName);
-
-      const message = "bambooSoop verify";
-
-      await registerWallet(userData, message, sig, account, walletName);
-
-      // if (walletName === "metamask") {
-      //   const isValid = verifyMessage({
-      //     message: message,
-      //     address: account,
-      //     signature: sig,
-      //   });
-
-      //   console.log("isValid : ", isValid);
-      // } else if (walletName === "kaikas") {
-      //   const _isValid = await validateSignedMessage(message, sig, account);
-      //   console.log("_isValid : ", _isValid);
-      // }
+      // console.log("w:", walletName);
     } catch (e) {
-      alert("지갑 등록에 실패하였습니다.");
-      console.log("checkRegister error", e);
+      // alert("지갑 등록에 실패하였습니다.");
+      Swal.fire({
+        icon: "error",
+        title: "register fail",
+        text: "지갑 등록에 실패하였습니다.",
+      });
+
+      // console.log("checkRegister error", e);
     }
   };
 
   const onClickSend = async () => {
     console.log("send!");
     console.log("userData:", userData);
+    console.log("registerdAddress:", registeredAddress);
 
-    registerWallet(userData);
+    // registerWallet(userData);
   };
 
   const registerWallet = async (
@@ -152,13 +155,28 @@ export const VerifyBamboo = () => {
     };
 
     axios.post(url, send_param).then((result) => {
-      // console.log(result.data.message);
-      // if (result.data.message) {
-      //   alert(result.data.message);
-      //   // window.location.href = "/community";
-      // } else {
-      //   alert("글쓰기 실패");
-      // }
+      // alert(result.data.message);
+
+      const message = result.data.message;
+      const isSuccess = result.data.isSuccess;
+
+      if (isSuccess) {
+        Swal.fire({
+          icon: "success",
+          title: "success",
+          text: message,
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "지갑 등록 실패",
+          text: message,
+        });
+      }
+
+      // console.log(result.data);
+
+      setRegisteredAddress(account);
     });
   };
 
@@ -179,11 +197,22 @@ export const VerifyBamboo = () => {
   }, [account]);
 
   useEffect(() => {
+    //receive iframe message
     window.addEventListener("message", async function (e) {
       if (e.origin === bambooURL) {
         const receivedData = e.data;
 
-        setUserData(receivedData);
+        // console.log("receivedData: ", receivedData);
+
+        const _userData = {
+          encryptedData: receivedData.encryptedData,
+          iv: receivedData.iv,
+        };
+
+        setUserData(_userData);
+        if (receivedData.walletAddress) {
+          setRegisteredAddress(receivedData.walletAddress);
+        }
 
         // console.log("data : ", receivedData);
       }
@@ -191,29 +220,66 @@ export const VerifyBamboo = () => {
   }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        margin: "100px 200px",
-        flexDirection: "column",
-        backgroundColor: "beige",
-      }}
-    >
-      <WalletConnect
-        getAccount={getAccount}
-        getProvider={getProvider}
-        getWalletName={getWalletName}
-      ></WalletConnect>
-      <div>
+    <div className="verifyBamboo-main-container">
+      <div className="verifyBamboo-container">
+        <div className="verifyBamboo-container_title">
+          <span>지갑 등록하기</span>
+        </div>
+      </div>
+      <div className="verifyBamboo-container">
+        <div className="verifyBamboo-container_description-container">
+          <div className="verifyBamboo-container_description">
+            <span>1. 지갑 연결하기를 눌러 지갑을 연결 합니다.</span>
+          </div>
+          <div className="verifyBamboo-container_description">
+            <span>2. 지갑 등록 버튼을 눌러 지갑을 밤부숲에 등록합니다.</span>
+          </div>
+        </div>
+      </div>
+      <div className="verifyBamboo-container">
+        <div className="verifyBamboo-container_button-container">
+          <div>
+            <WalletConnect
+              getAccount={getAccount}
+              getProvider={getProvider}
+              getWalletName={getWalletName}
+            ></WalletConnect>
+          </div>
+          <div>
+            <Button
+              className="verifyBamboo-registerWalletButton"
+              onClick={onClickRegisterWallet}
+              variant="success"
+            >
+              지갑 등록
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* <div>
         <Button onClick={handleSign}>Sign</Button>
+      </div> */}
+
+      {/* <div>{signature}</div> */}
+
+      {/* <div className="verifyBamboo-container">
+        <Button
+          className="verifyBamboo-registerWalletButton"
+          onClick={onClickRegisterWallet}
+          variant="success"
+        >
+          지갑 등록
+        </Button>
+      </div> */}
+      <div className="verifyBamboo-container">
+        <div className="verifyBamboo-container_registeredAddress-container">
+          <span>등록된 지갑 : {registeredAddress}</span>
+        </div>
       </div>
-      <div>{signature}</div>
-      <div>
-        <Button onClick={onClickRegisterWallet}>지갑 등록</Button>
-      </div>
-      <div>
+      {/* <div>
         <Button onClick={onClickSend}>send</Button>
-      </div>
+      </div> */}
     </div>
   );
 };
